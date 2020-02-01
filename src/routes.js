e9ab7fb0e1a8887df83f801a04c28c7425795c53
Dat Do <dat.b.do@gmail.com>
2020-02-01 16:16:29 -0500

    feat: added more route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import CommentIcon from "@material-ui/icons/Comment";
+import AssignmentIndSharpIcon from "@material-ui/icons/AssignmentIndSharp";
+import AccountTree from "@material-ui/icons/AccountTree";
 import Home from "./containers/Home";
+import { Typography, Button } from "@material-ui/core";
 
 const ROUTE_TYPE = {
   PUBLIC: "PUBLIC",
@@ -17,8 +22,56 @@ export default [
     type: ROUTE_TYPE.PUBLIC,
   },
   {
+    name: "Blogs",
+    icon: <CommentIcon />,
+    component: () => <div>Blogs</div>,
+    path: "/blogs",
+    exact: true,
+    type: ROUTE_TYPE.PUBLIC,
+  },
+  {
+    name: "Background",
+    icon: <AssignmentIndSharpIcon />,
+    component: () => <div>Experiences</div>,
+    path: "/experiences",
+    exact: true,
+    type: ROUTE_TYPE.PUBLIC,
+  },
+  {
+    name: "Projects",
+    icon: <AccountTree />,
+    component: () => <div>Projects</div>,
+    path: "/projects",
+    exact: true,
+    type: ROUTE_TYPE.PUBLIC,
+  },
+  {
+    name: "Login",
+    icon: <ExitToAppIcon />,
+    component: () => <div>Login</div>,
+    path: "/login",
+    exact: true,
+    type: ROUTE_TYPE.PRIVATE,
+  },
+  {
     name: "Error",
-    component: () => <div>Something Wrong</div>,
+    component: ({ history }) => (
+      <>
+        <Typography variant="h1" style={{ color: "red" }}>
+          404 - Error not found
+        </Typography>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            history.replace("/");
+          }}
+        >
+          Go Home
+        </Button>
+      </>
+    ),
     path: "*",
     hidden: true,
   },
