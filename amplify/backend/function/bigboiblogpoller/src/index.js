d0cda64f83126f3a050c59b66ef74c849eaff8c8
Dat Do <dat.b.do@gmail.com>
2021-09-23 22:32:53 -0400

    fix: initial integration with notion

--- amplify/backend/function/bigboiblogpoller/src/index.js
+++ amplify/backend/function/bigboiblogpoller/src/index.js
@@ -1,22 +1,47 @@
 /* Amplify Params - DO NOT EDIT
+	API_BIGBOIAPI_GRAPHQLAPIENDPOINTOUTPUT
+	API_BIGBOIAPI_GRAPHQLAPIIDOUTPUT
+	API_BIGBOIAPI_GRAPHQLAPIKEYOUTPUT
 	ENV
 	REGION
-	API_BIGBOIAPI_GRAPHQLAPIIDOUTPUT
-	API_BIGBOIAPI_GRAPHQLAPIENDPOINTOUTPUT
-	API_BIGBOIAPI_GRAPHQLAPIKEYOUTPUT
-	NOTION_INTEGRATION_TOKEN
+	STORAGE_BIGBOICONTENT_BUCKETNAME
 Amplify Params - DO NOT EDIT */
 
+const { Client } = require("@notionhq/client");
+const { get } = require("lodash");
+
+const RootBlockID = process.env.NOTION_BLOGPOST_BLOCKID;
+const auth = process.env.NOTION_INTEGRATION_TOKEN;
+
+const getNotionBlogPosts = async (client) => {
+    const rawData = await client.blocks.children.list({ block_id: RootBlockID });
+    return get(rawData, "results", []).filter(item => item.type === "child_page");
+}
+
+const retrievePageData = async (client, pageID) => {
+    const rawBlogPostData = await client.blocks.children.list({ block_id: pageID, page_size: 100 });
+    let next_cursor = get(rawBlogPostData, "next_cursor", null);
+    let has_more = get(rawBlogPostData, "has_more", false);
+    while (has_more && next_cursor) {
+        const nextData = await client.blocks.children.list({ block_id: pageID, page_size: 100, next_cursor });
+        get(rawBlogPostData, "results", []).push(...get(nextData, "results", []));
+        next_cursor = get(nextData, "next_cursor", null);
+        has_more = get(nextData, "has_more", false);
+    }
+    return rawBlogPostData
+}
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    const client = new Client({ 
+        auth
+    })
+    const notionBlogPosts = await getNotionBlogPosts(client);
+
+    const blogPosts = await Promise.all(notionBlogPosts.map(async blogPostOGData => {
+        const rawBlogPostData = await retrievePageData(client, get(blogPostOGData, "id"));
+        console.log(JSON.stringify(rawBlogPostData, null, 4));
+    }));
+
+    return { errors: [] };
 };
+
