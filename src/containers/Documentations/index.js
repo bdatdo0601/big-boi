b3dea4f683b80009eb51597f53a65dbcd269aa08
Dat Do <dat.b.do@gmail.com>
2021-10-21 23:15:56 -0400

    fix: enable api keys for external endpoint

--- src/containers/Documentations/index.js
+++ src/containers/Documentations/index.js
@@ -5,7 +5,7 @@ const DOCUMENTATION_URL = "https://docs.datbdo.com/";
 // 64 pixels are height of nav bar
 export default function Documentations() {
   return (
-    <div style={{ height: "calc(100vh - 64px)", overflow: "hidden" }}>
+    <div style={{ height: "calc(100vh - 64px)", overflow: "hidden", width: "100vw" }}>
       <iframe title="Documentation" src={DOCUMENTATION_URL} width="100%" height="100%" />
     </div>
   );
