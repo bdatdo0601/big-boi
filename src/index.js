7518db4139a70db99e59262231c67eb838284c01
Dat Do <bdatdo0601@dats-mbp.myfiosgateway.com>
2021-11-25 09:19:53 -0500

    fix: browser cache invalidation mechanism

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ClearBrowserCacheBoundary } from "react-clear-browser-cache";
+
 import hljs from "highlight.js/lib/core";
 import "./styles.css";
 import App from "./App";
@@ -9,7 +11,12 @@ import "highlight.js/styles/atom-one-dark.css";
 
 hljs.initHighlightingOnLoad();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ClearBrowserCacheBoundary auto fallback="Loading" duration={60000}>
+    <App />
+  </ClearBrowserCacheBoundary>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
