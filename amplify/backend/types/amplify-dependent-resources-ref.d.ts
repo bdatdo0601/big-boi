fd1b5de9cd2a6741f1c66bf06d143c1f7e7b7a02
Dat Do <bdatdo0601@dats-mbp.myfiosgateway.com>
2022-01-17 20:57:59 -0500

    fix: amplify infrastructure

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,88 @@
+export type AmplifyDependentResourcesAttributes = {
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string",
+            "CloudFrontDistributionID": "string",
+            "CloudFrontDomainName": "string",
+            "CloudFrontSecureURL": "string",
+            "CloudFrontOriginAccessIdentity": "string"
+        }
+    },
+    "auth": {
+        "bigboi327b1f0d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "storage": {
+        "bigboicontent": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "bigboiapi": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "bigboiexternalapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "function": {
+        "bigboibigboilambdalayer": {
+            "Arn": "string"
+        },
+        "bigboikinesisconsumer": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "bigboimessagestoring": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "bigboiblogpoller": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string",
+            "CloudWatchEventRule": "string"
+        },
+        "bigboiwebhookproxy": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "analytics": {
+        "bigboianalytics": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        },
+        "bigboidefaultkinesis": {
+            "kinesisStreamArn": "string",
+            "kinesisStreamId": "string",
+            "kinesisStreamShardCount": "string"
+        },
+        "bigboinotification": {
+            "snsTopicArn": "string",
+            "snsTopicName": "string"
+        }
+    }
+}
