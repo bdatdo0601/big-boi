8b7feec3f3f6b444253f60dfe23f1f7f3d4c7ced
Dat Do <bdatdo0601@dats-mbp.myfiosgateway.com>
2022-01-23 18:17:49 -0500

    fix: discord interaction

--- amplify/backend/function/bigboiutilitydiscordinteraction/src/index.js
+++ amplify/backend/function/bigboiutilitydiscordinteraction/src/index.js
@@ -11,26 +11,39 @@ const nacl = require('tweetnacl');
 // Your public key can be found on your application in the Developer Portal
 const PUBLIC_KEY = process.env.DISCORD_APPLICATION_PUBLIC_KEY;
 
+const isVerified = (evt) => {
+    try {
+        const signature = get(evt, "headers.x-signature-ed25519", "");
+        const timestamp = get(evt, "headers.x-signature-timestamp", "");
+        const rawBody = get(evt, "body"); // rawBody is expected to be a string, not raw bytes
+        return nacl.sign.detached.verify(
+            Buffer.from(timestamp + rawBody),
+            Buffer.from(signature, 'hex'),
+            Buffer.from(PUBLIC_KEY, 'hex')
+        );
+    } catch (err) {
+        console.warn("verification failed: ", err);
+        return false;
+    }
+} 
+
 exports.handler = async (event) => {
     console.log("Receive Event: ", event);
 
-    const signature = get(event, "headers.x-signature-ed25519", "");
-    const timestamp = get(event, "headers.x-signature-timestamp", "");
-    const rawBody = get(event, "body"); // rawBody is expected to be a string, not raw bytes
-    const isVerified = nacl.sign.detached.verify(
-        Buffer.from(timestamp + rawBody),
-        Buffer.from(signature, 'hex'),
-        Buffer.from(PUBLIC_KEY, 'hex')
-    );
-
-    if (!isVerified) {
+    if (!isVerified(event)) {
+        console.error("Event Not Verified");
         return {
             statusCode: 401,
-            body: "Invalid request signature"
+            body: "Invalid request signature",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            }, 
         }
     }
 
-    const requestData = JSON.parse(rawBody || "{}");
+    const requestData = JSON.parse(get(event, "body", "{}"));
+    console.log("request data: ", requestData)
     const result = {
         type: get(requestData, "type")
     }
