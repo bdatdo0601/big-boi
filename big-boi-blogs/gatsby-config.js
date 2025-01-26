889ea8f34a036fa45232c89ba68af747b00f042f
bdatdo0601 <dat.b.do@gmail.com>
2025-01-26 17:37:59 -0500

    fix: blogs

--- big-boi-blogs/gatsby-config.js
+++ big-boi-blogs/gatsby-config.js
@@ -30,14 +30,14 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-dynamodb",
+      resolve: "gatsby-source-awsv3-dynamodb",
       options: {
         typeName: "BlogPost",
         accessKeyId: process.env.BLOG_AWS_ACCESS_KEY,
         secretAccessKey: process.env.BLOG_AWS_SECRET_KEY,
         region: process.env.BLOG_AWS_REGION,
         params: {
-          TableName: process.env.BLOG_DYNAMODB_TABLE_NAME,
+          TableName: 'Post-y2hg5kdmyba6zmxbdmsd2wzzoy-dev',
         },
       },
     },
