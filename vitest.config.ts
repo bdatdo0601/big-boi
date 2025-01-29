716a79cdeb6755ce65bd39a07a117460d9c4bb84
bdatdo0601 <dat.b.do@gmail.com>
2025-01-29 12:06:51 -0500

    fix: vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    setupFiles: './src/setupTests.ts',
     include: ["./src/__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"]
   },
 });
