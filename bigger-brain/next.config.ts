2b3869d46527fca0a240ef15e886d1ff0977f510
bdatdo0601 <dat.b.do@gmail.com>
2025-02-06 21:54:22 -0500

    fix: bigger brain

--- bigger-brain/next.config.ts
+++ bigger-brain/next.config.ts
@@ -13,38 +13,6 @@ const withMDX = createMDX({
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
-  rewrites: async (): Promise<any> => {
-    return {
-      beforeFiles: [
-        // Handle blogs.datbdo.com
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'blogs.datbdo.com',
-            },
-            {
-              type: 'host',
-              value: 'localhost:3000'
-            }
-          ],
-          destination: '/blogs/:path*',
-        },
-        // Handle docs.datbdo.com
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'docs.datbdo.com',
-            },
-          ],
-          destination: '/docs/:path*',
-        },
-      ],
-    };
-  },
 };
 
 export default withMDX(nextConfig)
