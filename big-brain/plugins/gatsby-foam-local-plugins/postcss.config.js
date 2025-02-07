426a461c5d68ec75f133d1c37958d0544767fb76
bdatdo0601 <dat.b.do@gmail.com>
2025-02-06 21:59:35 -0500

    fix: delete big brain in favor of bigger brain

--- big-brain/plugins/gatsby-foam-local-plugins/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwind = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [
-    [tailwind({ config: `${__dirname}/tailwind.config.js` })],
-    autoprefixer,
-  ],
-};
--- big-brain/plugins/gatsby-foam-local-plugins/src/pages/404.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FourOFour = () => {
-  return <h1>Page Not Found</h1>;
-};
-
-export default FourOFour;
